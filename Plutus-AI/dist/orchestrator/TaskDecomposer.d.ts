dbd6d1c9463d692f636798af29260be8aaad0b39
Vishwajeet More <vishwajeetmore2023.comp@mmcoe.edu.in>
2025-11-30 12:08:31 +0530

    Delete Plutus-AI directory

--- Plutus-AI/dist/orchestrator/TaskDecomposer.d.ts
+++
@@ -1,7 +0,0 @@
-import { Task } from './WorkflowOrchestrator.js';
-export declare class TaskDecomposer {
-    decompose(goal: string): Promise<Task[]>;
-    private analyzeAndDecompose;
-    private decomposeMarketingCampaign;
-}
-//# sourceMappingURL=TaskDecomposer.d.ts.map
